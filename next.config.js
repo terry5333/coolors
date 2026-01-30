8842c490689e6620d36b219bcebd75c8cdbdbd56
terry5333 <terrylie0215@gmail.com>
2026-01-30 14:36:04 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   eslint: {
-    // 讓 build 不因為 eslint 掛掉（尤其在 CI/Vercel）
     ignoreDuringBuilds: true
   },
+
   typescript: {
-    // 如果你也想保險：避免 TS error 直接讓 build fail
-    // 先讓它上線跑，之後再慢慢修 TS
     ignoreBuildErrors: true
   }
 };
